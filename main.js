52de2c0b358b79523c7ffd0085dcc487ce69768c
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-01 09:10:11 -0800

    Search function working

--- main.js
+++ main.js
@@ -4,6 +4,24 @@ $(document).ready(function() {
   // check to make sure JS is loaded
   console.log('JS is loaded!');
   
-  // your code here
-
+  $('input').keydown(function handler(event) {
+  	if (event.which === 13) {
+  		event.preventDefault();
+  		var searchTerm = $('input').val();
+  		for (var i = 0; i < searchTerm.length; i++) {
+  			if (searchTerm[i] === " ") {
+  				searchTerm[i] = "+";
+  			}
+  		}
+	  	$.ajax({
+	  			method: 'GET',
+	  			url: 'http://api.spotify.com/v1/search?q=' + searchTerm + '&type=track',
+	  			success: function (response) {
+	    			response.tracks.items.forEach(function (element) {
+	    				$(".container").append("<p>" + element.name + " " + element.artists[0].name + "</p>");
+	    			});
+	  		}
+			});
+		}
+	});
 });
